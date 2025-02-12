1684ada69d383037c720e1f7b6cd55cc30eea188
Luca Bolognese <lucabol@microsoft.com>
2025-02-12 13:35:01 +0100

    Add script to merge HTML, CSS, and JS files into a single output file

---
+++ merge_files.py
@@ -0,0 +1,31 @@
+import os
+
+# Define file paths
+html_file = 'index.html'
+css_file = 'style.css'
+js_file = 'script.js'
+output_file = 'BVScouter.html'
+
+# Read the contents of the HTML file
+with open(html_file, 'r') as file:
+    html_content = file.read()
+
+# Read the contents of the CSS file
+with open(css_file, 'r') as file:
+    css_content = file.read()
+
+# Read the contents of the JS file
+with open(js_file, 'r') as file:
+    js_content = file.read()
+
+# Insert CSS into the HTML file
+html_content = html_content.replace('</head>', f'<style>{css_content}</style></head>')
+
+# Insert JS into the HTML file
+html_content = html_content.replace('</body>', f'<script>{js_content}</script></body>')
+
+# Write the merged content to the output file
+with open(output_file, 'w') as file:
+    file.write(html_content)
+
+print(f'Merged file created: {output_file}')
